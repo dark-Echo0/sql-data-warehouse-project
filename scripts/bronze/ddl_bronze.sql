f87d99f111cf270a4ed1ba14625b2c38f1bbf31b
Ric Araojo <ojoararico.me@gmail.com>
2025-09-02 21:27:18 +0800

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -22,7 +22,8 @@ CREATE TABLE bronze.crm_cust_info(
     cst_lastname NVARCHAR(50),
     cst_marital_status NVARCHAR(50),
     cst_gndr NVARCHAR(50),
-    cst_create_date DATE
+    cst_create_date DATE,
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 GO
 
@@ -37,7 +38,8 @@ CREATE TABLE bronze.crm_prd_info(
     prd_cost INT,
     prd_line NVARCHAR(50),
     prd_start_dt DATE,
-    prd_end_date DATE
+    prd_end_date DATE,
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 GO
 
@@ -54,7 +56,8 @@ CREATE TABLE bronze.crm_sls_details(
     sls_due_dt INT,
     sls_sales INT,	
     sls_quantity INT,
-    sls_price INT
+    sls_price INT,
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 GO
 
@@ -65,7 +68,8 @@ GO
 CREATE TABLE bronze.erp_cust_az12(
     cid VARCHAR(50),
     bdate DATE,
-    gen VARCHAR(50)
+    gen VARCHAR(50),
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 GO
 
@@ -74,8 +78,9 @@ IF OBJECT_ID('bronze.erp_loc_a101', 'U') IS NOT NUll
 GO
   
 CREATE TABLE bronze.erp_loc_a101(
-  cid VARCHAR(50),
-  cntry VARCHAR(50)
+	cid VARCHAR(50),
+	cntry VARCHAR(50),
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 GO
 
@@ -84,9 +89,10 @@ IF OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') IS NOT NUll
 GO
   
 CREATE TABLE bronze.erp_px_cat_g1v2(
-id VARCHAR(50),
-cat	VARCHAR(50),
-subcat VARCHAR(50),
-maintenance VARCHAR(50)
+	id VARCHAR(50),
+	cat	VARCHAR(50),
+	subcat VARCHAR(50),
+	maintenance VARCHAR(50),
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 GO
